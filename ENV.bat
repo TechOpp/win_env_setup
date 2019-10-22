7253f96e0c2cf78af3be0cb4f8f48785e9f1b215
sunnykrbit <sunnykr.bit@gmail.com>
2019-10-22 23:32:43 +0530

    Update ENV.bat
    Added java 9,10,11,12.13 and GCC of windows

--- ENV.bat
+++ ENV.bat
@@ -10,27 +10,27 @@ ECHO Done.
 EXIT /B
 
 :CASE_0
-  COLOR 0F
+  COLOR 0A
   set java_path=D:\Java\JDK\jdk0_32bit\bin
   set java_home=D:\Java\JDK\jdk0_32bit\
   GOTO END_CASE
 :CASE_1
-  COLOR 1F
+  COLOR 1B
   set java_path=D:\Java\JDK\jdk1_32bit\bin
   set java_home=D:\Java\JDK\jdk1_32bit\
   GOTO END_CASE
 :CASE_2
-  COLOR 2F
+  COLOR 2C
   set java_path=D:\Java\JDK\jdk2_32bit\bin
   set java_home=D:\Java\JDK\jdk2_32bit\
   GOTO END_CASE
 :CASE_3
-  COLOR 3F
+  COLOR 3D
   set java_path=D:\Java\JDK\jdk3_32bit\bin
   set java_home=D:\Java\JDK\jdk3_32bit\
   GOTO END_CASE
 :CASE_4  
-  COLOR 4F
+  COLOR 4E
   set java_path=D:\Java\JDK\jdk4_32bit\bin
   set java_home=D:\Java\JDK\jdk4_32bit\
   GOTO END_CASE
@@ -40,32 +40,57 @@ EXIT /B
   set java_home=D:\Java\JDK\jdk5_32bit\
   GOTO END_CASE
 :CASE_6-32
-  COLOR 6F
+  COLOR 6A
   set java_path=D:\Java\JDK\jdk6_32bit\bin
   set java_home=D:\Java\JDK\jdk6_32bit\
   GOTO END_CASE
  :CASE_6
-  COLOR 6F
+  COLOR 7B
   set java_path=D:\Java\JDK\jdk6_64bit\bin
   set java_home=D:\Java\JDK\jdk6_64bit\
   GOTO END_CASE 
 :CASE_7
-  COLOR 5F
+  COLOR 8C
   set java_path=D:\Java\JDK\jdk7_64bit\bin
   set java_home=D:\Java\JDK\jdk7_64bit\
   GOTO END_CASE 
 :CASE_8 
-  COLOR 8F
+  COLOR 9D
   set java_path=D:\Java\JDK\jdk8_64bit\bin
   set java_home=D:\Java\JDK\jdk8_64bit\
   GOTO END_CASE  
+:CASE_9 
+  COLOR 9E
+  set java_path=D:\Java\JDK\jdk9_64bit\bin
+  set java_home=D:\Java\JDK\jdk9_64bit\
+  GOTO END_CASE  
+:CASE_10
+  COLOR A1
+  set java_path=D:\Java\JDK\jdk10_64bit\bin
+  set java_home=D:\Java\JDK\jdk10_64bit\
+  GOTO END_CASE  
+:CASE_11 
+  COLOR B2
+  set java_path=D:\Java\JDK\jdk11_64bit\bin
+  set java_home=D:\Java\JDK\jdk11_64bit\
+  GOTO END_CASE  
+:CASE_12
+  COLOR C3
+  set java_path=D:\Java\JDK\jdk12_64bit\bin
+  set java_home=D:\Java\JDK\jdk12_64bit\
+  GOTO END_CASE  
+:CASE_13
+  COLOR D4
+  set java_path=D:\Java\JDK\jdk13_64bit\bin
+  set java_home=D:\Java\JDK\jdk13_64bit\
+  GOTO END_CASE    
 :DEFAULT_CASE
   cls
   ECHO JAVA %COLOR% does not available... Sorry Try another Version
   GOTO :EOF # return from CALL
 :END_CASE 
   
-  set common_path=c:\users\admin;D:\WEB_SERVERS\php;D:\WORK\NASM;D:\WORK\win-exTOOLS;D:\WORK\ASSEMBLER;D:\WEB_SERVERS\php\ComposerSetup;C:\Program Files\Git\bin
+  set common_path=c:\users\admin;D:\WORK\MinGW\bin;D:\WEB_SERVERS\php;D:\WORK\NASM;D:\WORK\win-exTOOLS;D:\WORK\ASSEMBLER;D:\WEB_SERVERS\php\ComposerSetup;C:\Program Files\Git\bin
   set window_path=C:\Program Files (x86)\PC Connectivity Solution\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\Program Files (x86)\AMD\ATI.ACE\Core-Static;C:\Program Files (x86)\MySQL\MySQL Utilities 1.3.6\;C:\FingerprintSensors\SMARTCHIP_DLL;C:\Program Files\Git\cmd;C:\ProgramData\ComposerSetup\bin;C:\Users\admin\AppData\Local\Microsoft\WindowsApps;C:\Users\admin\AppData\Roaming\Composer\vendor\bin
   set tools=D:\Java\apache-maven-3.2.5\bin;D:\Java\apache-ant-1.9.4\bin;D:\WEB_SERVERS\VHostServer\dev_mai\vendor\bin;D:\WEB_SERVERS\nodejs;C:\Users\admin\AppData\Roaming\npm;D:\WEB_SERVERS\OpenSSL-Win64\bin;D:\WEB_SERVERS\node_Apps\onsUI\Yarn\bin;D:\Java\dex2jar-2.0
   set server=D:\WEB_SERVERS\ApacheWebServer24\bin;D:\WEB_SERVERS\apache-tomcat-7.0.23\bin;D:\WEB_SERVERS\glassfish-4.1\bin;D:\WEB_SERVERS\jboss-as-7.1.1.Final\bin
@@ -79,7 +104,7 @@ EXIT /B
   set default_classpath=D:\WORK\JavaPractics;D:\Java\Project Drive\running project
 	
   set jar1=;D:\Java\LIB\C\
-  set default_jar=%jar1%servlet-api.jar%jar1%mysql.jar%jar1%ojdbc6.jar
+  set default_jar=%jar1%servlet-api.jar%jar1%mysql.jar%jar1%ojdbc6.jar%jar1%json-20171018.jar
   
   set jvm_mon=;D:\Java\lib\R\
   set jvm_state=%jvm_mon%basic.jar%jvm_mon%configurepolicy.jar%jvm_mon%jvmps.jar%jvm_mon%jvmsnap.jar%jvm_mon%jvmstat.jar%jvm_mon%jvmstat_graph.jar%jvm_mon%jvmstat_util.jar%jvm_mon%perf.jar%jvm_mon%perfagent.jar%jvm_mon%perfagentstubs.jar%jvm_mon%perfdata.jar%jvm_mon%visualgc.jar
@@ -93,15 +118,17 @@ EXIT /B
   set jme=;D:\Java\LIB\M\
   set jme_lib=%jme%cldc_1.8.jar%jme%configuration_1.0.jar%jme%device-io_1.0.jar%jme%device-io_1.1.jar%jme%device-io_1.1_adc.jar%jme%device-io_1.1_atcmd.jar%jme%device-io_1.1_counter.jar%jme%device-io_1.1_dac.jar%jme%device-io_1.1_generic.jar%jme%device-io_1.1_gpio.jar%jme%device-io_1.1_i2cbus.jar%jme%device-io_1.1_mmio.jar%jme%device-io_1.1_modem.jar%jme%device-io_1.1_pwm.jar%jme%device-io_1.1_spibus.jar%jme%device-io_1.1_uart.jar%jme%device-io_1.1_watchdog.jar%jme%gcf_1.8.jar%jme%httpclient_1.1.jar%jme%json_1.0.jar%jme%jsr75_1.0-fc.jar%jme%jsr120_1.1.jar%jme%jsr172_1.0.jar%jme%jsr177_1.0-apdu.jar%jme%jsr177_1.0-crypto.jar%jme%jsr179_1.0.jar%jme%jsr280_1.0.jar%jme%luikey-javadriver.jar%jme%meep_8.0.jar%jme%meep_8.0-cellular.jar%jme%meep_8.0-event.jar%jme%meep_8.0-io.jar%jme%meep_8.0-key.jar%jme%meep_8.0-lui.jar%jme%meep_8.0-media.jar%jme%meep_8.0-power.jar%jme%meep_8.0-rms.jar%jme%meep_8.0-swm.jar%jme%oauth2_1.0.jar%jme%runtime_update_1.0.jar%jme%satsa_extensions_api.jar%jme%ssl_extensions_api.jar%jme%compiler\me_plugin.jar%jme%ext\embedded-support-api.jar%jme%ext\security_api.jar
   
+  set and=;D:\Java\LIB\A\
+  set android=%and%android.jar%and%android-util-2.5.1.jar
   
-  set classpath=%classpath%;%default_classpath%%default_jar%%jvm_state%%spring%%hibernate%%jme_lib%
+  set classpath=%classpath%;%default_classpath%%default_jar%%jvm_state%%spring%%hibernate%%jme_lib%%android%
  
   set jvmstat_home=D:\Java\jvmstat(jvm-monitor)
   set jme_path=D:\Java\JDK\jme8_32bit\bin
   set path=%common_path%;%window_path%;%java_path%;%tools%;%jvmstat_home%\bat;%server%;%jme_path%
   
   
-  ECHO Now, Java %COLOR% ,JME, JVM Monitor, Maven, Ant, php, nasm, Spring, Hibernate, openssl, Nodejs are availables...  
+  ECHO Now, Java %COLOR% ,GCC, JME, JVM Monitor, Maven, Ant, php, nasm, Spring, Hibernate, openssl, Nodejs are availables...  
  
   VER > NUL # reset ERRORLEVEL
   GOTO :EOF # return from CALL
